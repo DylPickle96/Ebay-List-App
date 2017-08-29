2b9479174c9bab314f0a1e042b4002bdf79acf8b
Dylan Smith <dylan_smartguy@hotmail.com>
2017-08-28 22:19:45 -0700

    changed fetches to production

--- src/helpers/requests.js
+++ src/helpers/requests.js
@@ -1,17 +1,17 @@
 export const getList = () => {
-  return fetch ('http://localhost:3000/api/v1/list')
+  return fetch ('https://ebay-list-app-backend.herokuapp.com/api/v1/list')
   .then(res => res.json())
 }
 
 export const ebayRequest = () => {
-  return fetch ('http://localhost:3000/api/v1/ebay_request')
+  return fetch ('https://ebay-list-app-backend.herokuapp.com/api/v1/ebay_request')
 }
 
 export const postList = (data) => {
   const headers = new Headers({
     'Content-type': 'application/json'
   });
-  return fetch('http://localhost:3000/api/v1/list', {
+  return fetch('https://ebay-list-app-backend.herokuapp.com/api/v1/list', {
     method: 'POST',
     body: JSON.stringify({list: data}),
     headers
